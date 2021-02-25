16a20af791b8b6e95f2669e34df0fd99a55a7d75
woxane <77023555+woxane@users.noreply.github.com>
2021-02-25 15:17:22 +0330

    Add files via upload

---
+++ can you get the loop ?(5 kyu)/can you get the loop ? (5 kyu).py
@@ -0,0 +1,12 @@
+def loop_size(node):
+    onestep = node
+    twostep = node.next
+    while(onestep != twostep):
+        twostep = twostep.next.next
+        onestep = onestep.next
+    onestep = onestep.next
+    size = 1
+    while(onestep != twostep):
+        size += 1
+        onestep = onestep.next
+    return size
